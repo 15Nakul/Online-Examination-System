9c9541d05c7df5132f2d5d67392f42b7fbec3b76
Nakul Duvedi <nakulduvedi.cse19@chitkarauniversity.edu.in>
2022-06-19 01:17:17 +0530

    second

---
+++ exam/procfile
@@ -0,0 +1,1 @@
+web: gunicorn EXAMINATIONSYSTEM.wsgi --log-file -
--- onlinexam/urls.py
+++ onlinexam/urls.py
@@ -2,6 +2,8 @@ from django.urls import path,include
 from django.contrib import admin
 from exam import views
 from django.contrib.auth.views import LogoutView,LoginView
+from django.views.static import serve
+from django.conf.urls.static import static
 urlpatterns = [
    
     path('admin/', admin.site.urls),
@@ -47,6 +49,8 @@ urlpatterns = [
     path('admin-view-question', views.admin_view_question_view,name='admin-view-question'),
     path('view-question/<int:pk>', views.view_question_view,name='view-question'),
     path('delete-question/<int:pk>', views.delete_question_view,name='delete-question'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 
 ]
